4ba55683e65026b3c9eb691e664321698b4759e7
Joseph <JPMeehan5@Gmail.com>
2025-05-22 22:06:30 -0700

    Implemented new folder cli option

--- tools/pushLDBtoYML.mjs
+++ tools/pushLDBtoYML.mjs
@@ -4,15 +4,19 @@ import path from "path";
 
 const MODULE_ID = process.cwd();
 const yaml = true;
+const expandAdventures = true;
+const folders = true;
 
 const packs = await fs.readdir("./packs");
 for (const pack of packs) {
-  if (pack === ".gitattributes") continue;
+  if (pack === ".gitattributes" || pack === ".DS_Store") continue;
   console.log("Unpacking " + pack);
   const directory = `./src/packs/${pack}`;
   try {
     for (const file of await fs.readdir(directory)) {
-      await fs.unlink(path.join(directory, file));
+      const filePath = path.join(directory, file)
+      if (file.endsWith(yaml ? ".yml" : ".json")) await fs.unlink(filePath);
+      else fs.rm(filePath, { recursive: true })
     }
   } catch (error) {
     if (error.code === "ENOENT") console.log("No files inside of " + pack);
@@ -23,7 +27,9 @@ for (const pack of packs) {
     `${MODULE_ID}/src/packs/${pack}`,
     {
       yaml,
-      transformName
+      transformName,
+      expandAdventures,
+      folders,
     }
   );
 }
@@ -31,12 +37,26 @@ for (const pack of packs) {
  * Prefaces the document with its type
  * @param {object} doc - The document data
  */
-function transformName(doc) {
+function transformName(doc, context) {
   const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, "_");
-  const type = doc._key.split("!")[1];
+  let type = doc._key?.split("!")[1];
+  if (!type) {
+    if ("playing" in doc)
+      type = "playlist";
+    else if (doc.sorting)
+      type = `folder_${doc.type}`;
+    else if (doc.walls)
+      type = "scene";
+    else if (doc.results)
+      type = "rollTable";
+    else if (doc.pages)
+      type = "journal";
+    else
+      type = doc.type;
+  }
   const prefix = ["actors", "items"].includes(type) ? doc.type : type;
 
-  return `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${
-    yaml ? "yml" : "json"
-  }`;
+  let name = `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${yaml ? "yml" : "json"}`;
+  if ( context.folder ) name = path.join(context.folder, name);
+  return name;
 }
